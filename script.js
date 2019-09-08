d6d6c4e824f730badfe34a4f732884d8273332bf
Ben Eaves <ben@eaves.co.nz>
2019-09-08 17:18:42 +1200

    Added Kea card

--- script.js
+++ script.js
@@ -22,10 +22,31 @@ const App = new Vue({
           { text: 'The Kiwi are the only birds that have nostrils on the tip of their beak' },
           { text: 'Only found in the North Island' },
           { text: 'Feeds on insects mainly' },
-          { text: 'It has small wings but is flightless ' },
-          { text: 'Mainly a nocturnal bird ' },
+          { text: 'It has small wings but is flightless' },
+          { text: 'Mainly a nocturnal bird' },
         ]
       },
+      {
+        name: 'Kea',
+        status: 'Nationally Endangered',
+        level: 7,
+        image: "https://cdn.pixabay.com/photo/2017/03/12/23/31/kea-2138420_960_720.jpg",
+        threats: 
+        [
+          { text: 'Lead Poisoning' },
+          { text: 'Stoats' },
+          { text: 'Ferrets' },
+          { text: 'Dogs' },
+          { text: 'Motor Vehicles' }
+        ],
+        facts: 
+        [
+          { text: 'Kea are known well for their intelligence and curiosity' },
+          { text: 'Most wild Kea are estimated to live for 5 years' },
+          { text: 'Feeds on plants mainly, although they are known to eat human rubbish' },
+          { text: 'A loud and mischevious bird' },
+        ]
+      }
     ]
   }
 });
